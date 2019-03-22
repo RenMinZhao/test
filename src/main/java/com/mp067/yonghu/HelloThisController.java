dfd718733d5bf072b617ae19508eeafe7aafdb99
RenMinZhao <2306781698@qq.com>
2019-03-22 11:21:38 +0800

    test11.21

--- src/main/java/com/mp067/yonghu/HelloThisController.java
+++ src/main/java/com/mp067/yonghu/HelloThisController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-	
+public class HelloThisController {
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
         return "hello "+name+"，this is first messge";
-	    
+
     }
 }
