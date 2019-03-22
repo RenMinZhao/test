3b8fc8e15dc7eb15efa94015af9c0f4b503073dc
任泯曌 <46392933+RenMinZhao@users.noreply.github.com>
2019-03-22 11:19:24 +0800

    Update HelloController.java

--- src/main/java/com/mp067/yonghu/HelloController.java
+++ src/main/java/com/mp067/yonghu/HelloController.java
@@ -10,5 +10,6 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
         return "hello "+name+"，this is first messge";
+	    
     }
 }
